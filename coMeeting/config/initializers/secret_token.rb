9e9a9d545ca9fd6cf592d15e5103f91f1d351953
Joo Barbosa <j.barbosa333@gmail.com>
2011-10-02 15:16:45 +0100

    Project created.

---
+++ coMeeting/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoMeeting::Application.config.secret_token = '3a4bf774db633c5cf7a8a08c823833052ca5c928f9588489c4014cc7f54a50bb76c7a83757d1241b287c6094be06578e1d45a54745893a13817266e869f4fb1d'
