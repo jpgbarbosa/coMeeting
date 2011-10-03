61cee5b195b8a30a4a700938daa3913db87a2803
GuilhermeSimoes <GuilhermeMicaelo@gmail.com>
2011-10-04 00:49:44 +0100

    css added

--- coMeeting/config/routes.rb
+++ coMeeting/config/routes.rb
@@ -7,7 +7,6 @@ CoMeeting::Application.routes.draw do
 
   resources :users
 
-  get "home/index"
   get "/:page" => "static#show"
 
   # The priority is based upon order of creation:
@@ -59,7 +58,7 @@ CoMeeting::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'welcome#home'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
