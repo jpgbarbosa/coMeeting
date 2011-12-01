0de31840af9adb4c7dc99b18841e6ec21940f8ae
Joao Penetra <miguel.penetra@gmail.com>
2011-12-01 01:44:32 +0000

    corrected .yml files and migration files for deployment

--- coMeeting/db/migrate/20111107181347_change_is_attending_type_in_participations.rb
+++ coMeeting/db/migrate/20111107181347_change_is_attending_type_in_participations.rb
@@ -1,5 +1,6 @@
 class ChangeIsAttendingTypeInParticipations < ActiveRecord::Migration
   def change
-    change_column :participations, :is_attending, :integer
+    remove_column :participations, :is_attending
+    add_column :participations, :is_attending, :integer
   end
 end
