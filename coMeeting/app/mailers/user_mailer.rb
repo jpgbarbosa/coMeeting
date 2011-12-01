b2558c46fe860b7107c2ef75128e0e9d3db695e5
Joao Penetra <miguel.penetra@gmail.com>
2011-12-01 19:55:41 +0000

    merge

--- coMeeting/app/mailers/user_mailer.rb
+++ coMeeting/app/mailers/user_mailer.rb
@@ -2,11 +2,11 @@ class UserMailer < ActionMailer::Base
   default from: "coMeeting@comeeting.com"
 
   def admin_email(email, token)
-  	mail(:to => email, :subject => t("admin_subject", :default => "Your administration link"), :body => "http://localhost:3000/meetings/#{token}")
+  	mail(:to => email, :subject => t("admin_subject", :default => "Your administration link"), :body => "#{url_for(:host)}/meetings/#{token}")
   end
 
   def invitation_email(email, token)
-    mail(:to => email, :subject => t("participant_invitation", :default => "Meeting Invitation"), :body => "http://localhost:3000/meetings/#{token}")
+    mail(:to => email, :subject => t("participant_invitation", :default => "Meeting Invitation"), :body => "#{url_for(:host)}/meetings/#{token}")
   end
 
 end
